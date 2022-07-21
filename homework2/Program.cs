48c7741482b8bfb9ae4aeb041d0fd7d72ff7cfc4
«Denis» <denis.ch.9797@mail.ru>
2022-07-22 01:11:12 +0500

    homeworks9

---
+++ homework2/Program.cs
@@ -0,0 +1,11 @@
+﻿//Напишите программу, которая найдёт сумму 
+//натуральных элементов в промежутке от N до M
+
+int n, m, s=0;
+Console.Write("Введи значение от N: ");
+n = Convert.ToInt16(Console.ReadLine());
+Console.Write("Введи значение до M: ");
+m = Convert.ToInt16(Console.ReadLine());
+for (int i = n; i <= m; i++)
+    s += i;
+Console.WriteLine(s);
